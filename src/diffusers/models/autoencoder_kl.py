5a6bbd1c527797d00a0ed63189338ea93a2cb073
zejietian <109349505+zejietian@users.noreply.github.com>
2023-03-01 14:20:19 +0800

    Update autoencoder_kl.py

--- src/diffusers/models/autoencoder_kl.py
+++ src/diffusers/models/autoencoder_kl.py
@@ -148,7 +148,7 @@ class AutoencoderKL(ModelMixin, ConfigMixin):
     @apply_forward_hook
     def decode(self, z: torch.FloatTensor, return_dict: bool = True) -> Union[DecoderOutput, torch.FloatTensor]:
         if self.use_slicing and z.shape[0] > 1:
-            decoded_slices = [self._decode(z_slice).sample for z_slice in z.split(1)]
+            decoded_slices = map(lambda z_slice: self._decode(z_slice).sample, z.split(1))
             decoded = torch.cat(decoded_slices)
         else:
             decoded = self._decode(z).sample
