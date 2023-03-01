f6a7da5fa1728466c214d412ce483fa557dd2d6c
zejietian <109349505+zejietian@users.noreply.github.com>
2023-03-01 14:36:35 +0800

    Update resnet.py

--- src/diffusers/models/resnet.py
+++ src/diffusers/models/resnet.py
@@ -532,52 +532,48 @@ class ResnetBlock2D(nn.Module):
             )
 
     def forward(self, input_tensor, temb):
-        hidden_states = input_tensor
-
         if self.time_embedding_norm == "ada_group":
-            hidden_states = self.norm1(hidden_states, temb)
+            self.norm1(input_tensor,temb,inplace=True)
         else:
-            hidden_states = self.norm1(hidden_states)
+            self.norm1(input_tensor,inplace=True)
 
-        hidden_states = self.nonlinearity(hidden_states)
+        input_tensor = self.nonlinearity(input_tensor,inplace=True)
 
         if self.upsample is not None:
             # upsample_nearest_nhwc fails with large batch sizes. see https://github.com/huggingface/diffusers/issues/984
             if hidden_states.shape[0] >= 64:
                 input_tensor = input_tensor.contiguous()
-                hidden_states = hidden_states.contiguous()
             input_tensor = self.upsample(input_tensor)
-            hidden_states = self.upsample(hidden_states)
         elif self.downsample is not None:
             input_tensor = self.downsample(input_tensor)
-            hidden_states = self.downsample(hidden_states)
 
-        hidden_states = self.conv1(hidden_states)
+       self.conv1(input_tensor,out=input_tensor)
 
         if self.time_emb_proj is not None:
             temb = self.time_emb_proj(self.nonlinearity(temb))[:, :, None, None]
 
         if temb is not None and self.time_embedding_norm == "default":
-            hidden_states = hidden_states + temb
+            input_tensor.add_(temb)
 
         if self.time_embedding_norm == "ada_group":
-            hidden_states = self.norm2(hidden_states, temb)
+            self.norm2(input_tensor, temb, inplace=True)
         else:
-            hidden_states = self.norm2(hidden_states)
+            self.norm2(input_tensor, inplace=True)
 
         if temb is not None and self.time_embedding_norm == "scale_shift":
             scale, shift = torch.chunk(temb, 2, dim=1)
-            hidden_states = hidden_states * (1 + scale) + shift
+            input_tensor.mul_(1 + scale).add_(shift)
 
-        hidden_states = self.nonlinearity(hidden_states)
+      
 
-        hidden_states = self.dropout(hidden_states)
-        hidden_states = self.conv2(hidden_states)
+        input_tensor = self.nonlinearity(input_tensor, inplace=True)
+        input_tensor = self.dropout(input_tensor)
+        
+        self.conv2(input_tensor, out=input_tensor)
 
         if self.conv_shortcut is not None:
-            input_tensor = self.conv_shortcut(input_tensor)
-
-        output_tensor = (input_tensor + hidden_states) / self.output_scale_factor
+             self.conv_shortcut(input_tensor, out=input_tensor)
+        input_tensor.div_(self.output_scale_factor)
 
         return output_tensor
 
